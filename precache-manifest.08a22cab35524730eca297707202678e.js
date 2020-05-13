3fd782066be756e4e3847e9b8504b793cf0fa4c7
shiikong <shiikong@naver.com>
2020-05-13 20:52:46 +0900

    Updates

---
+++ precache-manifest.08a22cab35524730eca297707202678e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f12c8cfb7b3551c9bad935a21d619b",
+    "url": "/REACT_movie-app-2020/index.html"
+  },
+  {
+    "revision": "59f8e241ed308f4a34e0",
+    "url": "/REACT_movie-app-2020/static/js/2.5f232f70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT_movie-app-2020/static/js/2.5f232f70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212de5b16ff3edb0d235",
+    "url": "/REACT_movie-app-2020/static/js/main.cd7766a8.chunk.js"
+  },
+  {
+    "revision": "7fd9b62972c8e3438358",
+    "url": "/REACT_movie-app-2020/static/js/runtime-main.c58a4085.js"
+  }
+]);
